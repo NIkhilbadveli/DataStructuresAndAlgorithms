05d904fa8d09112bc23a0d9b1db33b0ba7a0aade
Nikhil Reddy <nikhilbadveli6@gmail.com>
2022-03-15 21:59:16 +0000

    insertion sort

--- Algorithms/insertion_sort.py
+++ Algorithms/insertion_sort.py
@@ -12,3 +12,25 @@ Best case = O(n)
 
 Space complexity is O(n)"""
 
+
+def sort_iteratively(arr):
+    """
+    Uses the above algorithm.
+    Time complexity = O(n^2) since there are 2 for loops.
+    :param arr:
+    :return:
+    """
+    sorted_arr = [arr[0]]
+    sorted_arr.insert(0, arr[1]) if arr[1] < sorted_arr[0] else sorted_arr.append(arr[1])
+    for i in range(2, len(arr)):
+        for j in range(len(sorted_arr)):
+            if arr[i] <= sorted_arr[j]:
+                sorted_arr.insert(j, arr[i])
+                break
+            elif sorted_arr[j] < arr[i] < sorted_arr[j + 1]:
+                sorted_arr.insert(j + 1, arr[i])
+                break
+            elif j == len(sorted_arr) - 2:
+                sorted_arr.append(arr[i])
+                break
+    return sorted_arr
