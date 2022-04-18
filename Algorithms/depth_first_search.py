8ceca3ad0cd74c136c3c7aacf9560e862af94e03
Nikhil Reddy <nikhilbadveli6@gmail.com>
2022-04-18 15:55:53 +0100

    bfs and dfs

--- Algorithms/depth_first_search.py
+++
@@ -1,9 +0,0 @@
-"""
-Used with binary trees*
-
-At each node, the DFS will descend as far down the left branch as possible until it reaches a leaf.
-Then it will step back, go down one right branch, and then back to left branches.
-It sticks to the left until it reaches a leaf, and then it steps back and goes right.
-
-# breadth_first_search
-"""
